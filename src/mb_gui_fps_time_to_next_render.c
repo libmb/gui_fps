4eec5170d3aa58af0f3acdb64dd1c6075fc04fd9
맹주영 <mjy9088@naver.com>
2024-04-08 00:43:57 +0900

    fix incorrect code

--- src/mb_gui_fps_time_to_next_render.c
+++ src/mb_gui_fps_time_to_next_render.c
@@ -6,13 +6,11 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 17:29:56 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2024/04/08 00:02:59 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2024/04/08 00:40:15 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mb_gui_fps.h"
-
-#include <unistd.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 
@@ -33,15 +31,27 @@
 t_err	mb_gui_fps_time_to_next_render(t_mb_gui_fps *self, uint32_t *out)
 {
 	struct timeval	tv;
-	time_t			sec_difference;
-	suseconds_t		usec_difference;
+	struct timeval	next_render;
 
 	if (gettimeofday(&tv, NULL))
 		return (true);
-	sec_difference = tv.tv_sec - self->last_rendered_time.tv_sec;
-	usec_difference = 1000000 + tv.tv_usec - self->last_rendered_time.tv_usec;
-	*out
-		= (uint32_t)(sec_difference * 1000000 + usec_difference - 1000000);
+	next_render.tv_sec = self->last_rendered_time.tv_sec
+		+ (time_t)self->us_per_frame / 1000000;
+	next_render.tv_usec = self->last_rendered_time.tv_usec
+		+ (suseconds_t)self->us_per_frame % 1000000;
+	if (next_render.tv_usec >= 1000000)
+	{
+		next_render.tv_sec += 1;
+		next_render.tv_usec -= 1000000;
+	}
+	if (tv.tv_sec > next_render.tv_sec || (tv.tv_sec == next_render.tv_sec
+			&& tv.tv_usec > next_render.tv_usec))
+	{
+		*out = 0;
+		return (false);
+	}
+	*out = (uint32_t)(tv.tv_sec - self->last_rendered_time.tv_sec * 1000000
+			+ tv.tv_usec - self->last_rendered_time.tv_usec);
 	return (false);
 }
 
